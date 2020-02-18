9020a311aace43a225f2701f8d7ef0f724d9f67a
Víctor <victordf2005@gmaillcom>
2020-02-18 22:31:54 +0100

    commit backup

---
+++ src/main/java/clases/InformeStock.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author clatvdf
+ */
+public class InformeStock {
+    
+    private ArrayList<InformeTendas> informeTendas;
+
+    public InformeStock() {
+    }
+
+    public InformeStock(ArrayList<InformeTendas> informeTendas) {
+        this.informeTendas = informeTendas;
+    }
+    
+}
